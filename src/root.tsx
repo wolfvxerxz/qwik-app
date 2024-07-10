524945627b7b66e98bf77a584045575f6d011244
Wolf <wolfvxerxz@gmail.com>
2024-07-10 16:08:11 +0200

    init

--- src/root.tsx
+++ src/root.tsx
@@ -7,6 +7,7 @@ import {
 import { RouterHead } from "./components/router-head/router-head";
 import { isDev } from "@builder.io/qwik/build";
 
+
 import "./global.css";
 
 export default component$(() => {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,9 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
+    <div>
       <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
+    </div>
   );
 });
 
